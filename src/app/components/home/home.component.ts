cf6548c594cdbede886ced27fcfc58f3878f777e
ilkosak <ikoliogi@gmail.com>
2019-12-11 19:36:04 +0200

    first commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -27,19 +27,21 @@ export class HomeComponent implements OnInit {
 
   public getWeather(city){
 
-      this.http.get('https://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=API_KEY')
-      .subscribe(res => {
-        if(!res.error) {
+      this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=API_KEY')
+      .subscribe((res: any) => {
+        if (!res.error) {
           this.weather = res;
           this.weatherDesc = res.weather[0].description;
           this.temperature = Math.trunc(res.main.temp - 273.15);
           this.maxTemperature = Math.trunc(res.main.temp_max - 273.15);
           this.minTemperature = Math.trunc(res.main.temp_min - 273.15);
           this.hideTable = false;
-          this.mycity = city[0].toUpperCase()+city.slice(1);
+          this.mycity = city[0].toUpperCase() + city.slice(1);
           console.log(res);
         }
       });
   }
+
+  // sthn html mporoume na typwsoume tis metavlhtes ws ekshs: res?.main?.temp gia na apofugoume to provlhma
 
 }
